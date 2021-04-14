10d015bb81ce91c8e4fb7d825f426cae77ffb038
Gutyiii <gutyeszbandesz@gmail.com>
2021-04-14 11:01:38 +0200

    nev, email, email ujra html+js

---
+++ public_html/urlapkezeles.js
@@ -0,0 +1,54 @@
+window.addEventListener("load", init);
+
+function ID(nev) {
+    return document.getElementById(nev);
+}
+
+function $(nev) {
+    return document.querySelectorAll(nev);
+}
+
+function init() {
+//    console.log("hahó");
+    ID("kuld").addEventListener("click", validalas);
+}
+
+function validalas() {
+//    console.log("validálás");
+//    név beviteli ellenőrzés: nagybetűvel kezdődik?, 
+//    vannak e benne szóközök?, 
+//    betűt adott e meg?,
+//    1 karakternél hosszabb legyen!
+    var urlapadatok = "";
+    var hiba = "";
+    var filter = /[A-Z]+[a-z]{2,}/;
+    var emailfilter= /[*a-z]+@+[*a-z]+.+[*a-z]/;
+    if (!filter.test(ID("nev").value)) {
+        hiba += "A név nagybetűvel kezdődjön és legalább három karakter legyen! \n\
+                Legalább három karakter hosszú legyen a név!";
+        ID("nev").style.border = "3px solid red";
+    } else {
+        ID("nev").style.border = "none";
+        urlapadatok += "Név: " + ID("nev").value + "<br>";
+    }
+    
+    if (!emailfilter.test(ID("email").value)) {
+        hiba += "Az kisbetűvel kezdődjön, legyen benne kukac, következzen utána kisbetű, legyen egy pont és végül még néhány kisbetű.";
+        ID("email").style.border = "3px solid red";
+    } else {
+        ID("email").style.border = "none";
+        urlapadatok += "Email: " + ID("email").value + "<br>";
+    }
+    
+    if (!emailfilter.test(ID("emailujra").value)) {
+        hiba += "Az kisbetűvel kezdődjön, legyen benne kukac, következzen utána kisbetű, legyen egy pont és végül még néhány kisbetű.";
+        ID("emailujra").style.border = "3px solid red";
+    } else {
+        ID("emailujra").style.border = "none";
+        urlapadatok += "Email űjra: " + ID("emailujra").value + "<br>";
+    }
+    
+    console.log(hiba);
+    $("aside section:nth-child(1) p")[0].innerHTML = hiba;
+    $("aside section:nth-child(2) p")[0].innerHTML = urlapadatok;
+}
